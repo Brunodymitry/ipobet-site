a925b47a82171163dc34e3cbe2ab3620f15bfc7c
Brunodymitry <cbrunopositivo99@gmail.com>
2025-07-13 19:24:13 -0300

    Delete login.js

--- login.js
+++
@@ -1,8 +0,0 @@
-function login() {
-  const user = document.getElementById('username').value;
-  if (user) {
-    localStorage.setItem('nipobet_user', user);
-    localStorage.setItem('nipobet_balance', 1000); // saldo inicial
-    window.location.href = "apostas.html";
-  }
-}
